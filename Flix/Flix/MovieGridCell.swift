e51f88415bafa72ff6ec079d925d7e9944eef54f
Leo (Shih-Chuan) Ku <leo.sc.ku@gmail.com>
2019-02-24 22:58:02 -0800

    Unit 2 main finish

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Leo Ku on 2/24/19.
+//  Copyright © 2019 Shih-Chuan Leo Ku. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+  @IBOutlet weak var posterView: UIImageView!
+}
